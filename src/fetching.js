c2620e8004a627fc495c9c7ea47c24fce9154206
pad <pd.development@protonmail.com>
2026-06-24 14:54:50 +0200

    Added fetch functionality to the loadNewJoke- button

--- src/fetching.js
+++ src/fetching.js
@@ -3,6 +3,6 @@ const API_ENDPOINT = "https://witzapi.de/api/joke";
 export async function fetchJoke() {
   const response = await fetch(API_ENDPOINT);
   const data = await response.json();
-
+    return data[0].text;
   console.log(data);
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 import { fetchJoke } from "./fetching";
 
-console.log(await fetchJoke());
+const currentJokeElement = document.querySelector(".current-joke__text");
+const loadNewJokeButton = document.querySelector(
+  ".current-joke__button-generate",
+);
+
+async function loadNewJoke() {
+  const joke = await fetchJoke();
+
+  currentJokeElement.innerText = joke;
+}
+
+loadNewJokeButton.addEventListener("click", loadNewJoke);
